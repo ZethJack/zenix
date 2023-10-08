a1838c6a63a32fe9bf936d565b10f5a707728872
= <=>
2023-10-08 17:27:02 +0200

    Moved configuration into hosts subfolder

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
         specialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main nixos configuration file <
-          ./nixos/configuration.nix
+          ./hosts/hashbrown/configuration.nix
         ];
       };
     };
